bb1a430e1006d481f93b8ada429d9a4333caeb9a
ferdlg <ferdlgpar@gmail.com>
2024-12-11 18:55:42 -0500

    [feat] - configuration and installation of the app

---
+++ BotanicStore/src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import './assets/styles/utils/global.css'
+import App from './App.jsx'
+import {NextUIProvider} from '@nextui-org/react'
+
+createRoot(document.getElementById('root')).render(
+
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+)
